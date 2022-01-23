601e73b26094576f5fcda17348503891e0da5de0
Almira M Anderson <39601411+almiracma@users.noreply.github.com>
2022-01-23 14:14:16 -0800

    verified server running

--- server.js
+++ server.js
@@ -2,18 +2,41 @@
 projectData = {};
 
 // Express to run server and routes
-
+const express = require("express");
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
+const bodyParser = require("body-parser");
+
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static("website"));
 
 // Spin up the server
+const port = 8000;
+
+const server = app.listen(port, () => {
+  console.log(`Listening at : ${port}`);
+});
+
+// longer way to write the function
+
+// const server = app.listen(port, listening);
+
+// function listening() {
+//     console.log(`Listening at: ${port}`)
+// }
+
 // Callback to debug
 
 // Initialize all route with a callback function
